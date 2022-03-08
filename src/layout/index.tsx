63b0843729f206a5728f7ab40bad88ba32974737
woowalker <qiangx.job@gmail.com>
2022-03-08 17:20:54 +0800

    feat: 优化未登录页跳转逻辑到 layout/index.tsx 组件

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,44 @@
+import { useEffect } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+import { commonStore } from 'src/stores'
 import Main from './Main'
+import { $api, $consts } from 'src/plugins'
 
 const Layout = (props: any) => {
+  const history = useHistory()
+
+  const location = useLocation()
+  useEffect(() => {
+    if (location.pathname !== $consts['ROUTE/LOGIN']) {
+      const toLogin = (loginModel: any) => {
+        const { ORG_ID } = commonStore.userinfo
+        if (loginModel && !ORG_ID) {
+          const { username, password, orgId } = loginModel
+          $api['common/login']({
+            Parameter1: username,
+            Parameter2: password,
+            Parameter4: orgId
+          }).then((res: any) => {
+            if (res) {
+              commonStore.setLoginData(res)
+              $api['common/getAllEnum']()
+            }
+          })
+        }
+      }
+
+      commonStore.getLoginModel()
+        .then((model: any) => {
+          const { orgId } = model
+          if (orgId) {
+            toLogin(model)
+          } else {
+            history.push($consts['ROUTE/LOGIN'])
+          }
+        })
+    }
+  }, [location])
+
   return <Main>{props.children}</Main>
 }
 
--- src/modules/main/index.tsx
+++ src/modules/main/index.tsx
@@ -1,47 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { observer } from 'mobx-react'
 import { matchPath } from 'react-router-dom'
 import { commonStore, appStore } from 'src/stores'
 import { FormPage } from 'src/components'
 import { Notice } from 'src/modules/main/components'
-import { $api, $consts } from 'src/plugins'
+import { $consts } from 'src/plugins'
 
 const FakePage = () => {
   return null
 }
 
 const Main = (props: any) => {
-  const [loginModel, setLoginModel] = useState()
-
-  useEffect(() => {
-    commonStore.getLoginModel()
-      .then((model: any) => {
-        const { orgId } = model
-        if (orgId) {
-          setLoginModel(model)
-        } else {
-          props.history.push($consts['ROUTE/LOGIN'])
-        }
-      })
-  }, [])
-
-  useEffect(() => {
-    const { ORG_ID } = commonStore.userinfo
-    if (loginModel && !ORG_ID) {
-      const { username, password, orgId } = loginModel
-      $api['common/login']({
-        Parameter1: username,
-        Parameter2: password,
-        Parameter4: orgId
-      }).then((res: any) => {
-        if (res) {
-          commonStore.setLoginData(res)
-          $api['common/getAllEnum']()
-        }
-      })
-    }
-  }, [loginModel])
-
   useEffect(() => {
     const matchMenu = matchPath<any>(props.location.pathname, {
       path: $consts['ROUTE/MAIN'],
